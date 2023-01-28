9a3492271e79260806f17193f67c9fea29405d3f
jaczhi <54284925+jaczhi@users.noreply.github.com>
2023-01-28 13:14:44 -0800

    Add home and login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NotFound from './pages/NotFound/NotFound';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 import {
   makeUser,
   signIn,
@@ -10,10 +13,14 @@ import {
   userLoggedIn,
 } from "./firebase/account"
 
+
+
 function App() {
   return (
     <div className="App">
         <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
     </div>
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Alert, Container } from 'react-bootstrap';
+
+function Login (props) {
+    return (
+        <div>
+            <Container>
+            <Alert key="primary">
+                Foo bar baz!!!!! This is Login page!!
+            </Alert>
+            </Container>
+        </div>
+    );
+}
+
+export default Login;
