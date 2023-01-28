dc94b8599437293f2b33bfc5c2dff51e63c7db10
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-01-28 12:35:53 -0800

    errorless authentication starter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound/NotFound';
+import {
+  makeUser,
+  signIn,
+  signOutUser,
+  getUserId,
+  userLoggedIn,
+} from "./firebase/account"
 
 function App() {
   return (
